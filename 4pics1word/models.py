891f5da606f6d85d0c77f32be1c72b6988b0daf9
ashkaan singh <ashkaan.singh@gmail.com>
2024-04-07 15:25:48 +0800

    Added User class for database, includes columns for id, username, password and email

--- 4pics1word/models.py
+++ 4pics1word/models.py
@@ -3,3 +3,9 @@ from flask_login import UserMixin
 
 db = SQLAlchemy(app)
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, nullable = False, unique = True)
+    password = db.Column(db.String, nullable = False)
+    email = db.Column(db.String, nullable = False, unique = True)
+
