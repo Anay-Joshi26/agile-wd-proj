84bba75ab1d6cd4938538f051f32a51e6ef92976
ashkaan singh <ashkaan.singh@gmail.com>
2024-04-08 22:40:13 +0800

    Created databse.db to hold data and added configurations to test to create the database and the user and game model

--- 4pics1word/models.py
+++ 4pics1word/models.py
@@ -1,7 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
+from main import app
+
 db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db.create_all()
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
@@ -9,3 +14,13 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String, nullable = False)
     email = db.Column(db.String, nullable = False, unique = True)
 
+class Game(db.Model):
+    gameId = db.column(db.Integer, primary_key = True)
+    answer = db.Column(db.String, nullable = False)
+    clue1 = db.Column(db.String, nullable = False)
+    clue2 = db.Column(db.String, nullable = False)
+    clue3 = db.Column(db.String, nullable = False)
+    image1 = db.Column(db.LargeBinary, nullable = False)
+    image2 = db.Column(db.LargeBinary, nullable = False)
+    image3 = db.Column(db.LargeBinary, nullable = False)
+    image4 = db.Column(db.LargeBinary, nullable = False)
