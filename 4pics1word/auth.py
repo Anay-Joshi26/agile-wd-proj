b1991cbd8cac6341b8d0d6320feb0b133d026c74
Anay <you@example.com>
2024-04-17 16:50:16 +0800

    new branch for login validation, to give a user feedback about whether they have entered invalid or incorrect values when logging in. This is partly done client side, but also done server side. To achieve a seemless experience for the user changes changes have been made which now sends a request to /login or /register, which then responds back with the appropriate message/success

---
+++ 4pics1word/auth.py
@@ -0,0 +1,37 @@
+from flask import redirect, url_for, render_template
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+import re
+
+from models import db, User
+
+def validate_username(username):
+    return User.query.filter_by(username=username).first()
+
+def register_new_user(username, password, bcrypt):
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_user = User(username = username, password = hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+def login_new_user(username, password, bcrypt):
+    user = validate_username(username) 
+    if user:
+        if bcrypt.check_password_hash(user.password, password):
+            print(user.username)
+            login_user(user)
+            return True
+    
+    return False
+    
+def isValidUsername(username):
+    return re.match(r'^[a-zA-Z0-9_]+$', username)
+
+def isValidPassword(password):
+    return re.match(r'^.+$', password)
+
+
+    
+
+
+    
