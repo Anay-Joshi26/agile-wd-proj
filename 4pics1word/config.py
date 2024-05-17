f77cbc7cf1aac82b45dfaa74cac5fff50e05317a
Anay <you@example.com>
2024-05-17 11:48:14 +0800

    added the SECRET KEY as part of a .env file so it was not mentioned in our py file as text. This .env file was added into gitignore

--- 4pics1word/config.py
+++ 4pics1word/config.py
@@ -1,12 +1,15 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
+    SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     UPLOAD_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), 'uploads'))
 
 class TestConfig:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory'
     UPLOAD_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), 'uploads'))
     TESTING = True
